4f1ee3c6cf558c1c965553078d524553540f649d
Arditto <arditto.trianggada@corp.bri.co.id>
2017-11-28 02:22:58 +0700

    2017/11/27 18.21 WIB

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ class_bucketlists(app)
 class_ex1(app)
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host=os.getenv('FLASK_HOST'), port=int(os.getenv('FLASK_PORT')))
+    # app.run()
 # curl http://127.0.0.1:5000/
 # curl -X GET http://127.0.0.1:5000/ex1
 # curl -X POST -H "Content-Type: application/json" -d '{"amount":7000, "description":"bonuslala", "id":2}' http://127.0.0.1:5000/ex1
